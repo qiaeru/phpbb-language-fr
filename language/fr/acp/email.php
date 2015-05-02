3e962d43e0f04c49a144fd998e009a5896bba415
Maël Soucaze <maelsoucaze@gmail.com>
2015-05-02 08:46:58 +0200

    Update file details.

--- language/fr/acp/email.php
+++ language/fr/acp/email.php
@@ -1,15 +1,15 @@
 <?php
 /**
 *
-* This file is part of the French Language Package for the
-* phpBB Forum Software.
+* This file is part of the official French Language Package
+* for the phpBB Forum Software.
 *
 * @copyright (c) phpBB Limited <https://www.phpbb.com>
 * @copyright (c) Maël Soucaze <https://www.maelsoucaze.com>
 * @license GNU General Public License, version 2 (GPL-2.0)
 *
-* For information about the language package, please see the
-* AUTHORS, LICENSE ans VERSION files.
+* For information about the language package, please see
+* https://www.phpbb.com/customise/db/translation/french/
 *
 */
 
