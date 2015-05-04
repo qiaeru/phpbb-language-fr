708d78fb37505dd6ce156250d86b9663c557bf0d
Maël Soucaze <maelsoucaze@gmail.com>
2015-05-04 23:09:44 +0200

    Update file details and add language strings.

--- language/fr/migrator.php
+++ language/fr/migrator.php
@@ -1,15 +1,15 @@
 <?php
 /**
 *
-* This file is part of the French Language Package for the
-* phpBB Forum Software.
+* This file is part of the official French Language Package
+* for the phpBB Forum Software.
 *
 * @copyright (c) phpBB Limited <https://www.phpbb.com>
 * @copyright (c) Maël Soucaze <https://www.maelsoucaze.com>
 * @license GNU General Public License, version 2 (GPL-2.0)
 *
-* For information about the language package, please see the
-* AUTHORS, LICENSE ans VERSION files.
+* For information about the language package, please see
+* https://www.phpbb.com/customise/db/translation/french/
 *
 */
 
@@ -53,6 +53,13 @@ $lang = array_merge($lang, array(
 	'MIGRATION_NOT_VALID'				=> '%s n’est pas une migration valide.',
 	'MIGRATION_SCHEMA_DONE'				=> 'Schémas installés : %1$s ; durée : %2$.2f secondes',
 	'MIGRATION_SCHEMA_RUNNING'			=> 'Installation des schémas : %s.',
+
+	'MIGRATION_INVALID_DATA_MISSING_CONDITION'		=> 'Une migration n’est pas valide. Une condition dans une instruction d’aide « IF » est manquant.',
+	'MIGRATION_INVALID_DATA_MISSING_STEP'			=> 'Une migration n’est pas valide. Un rappel valide à une étape de migration dans une instruction d’aide « IF » est manquant.',
+	'MIGRATION_INVALID_DATA_CUSTOM_NOT_CALLABLE'	=> 'Une migration n’est pas valide. Une fonction de rappel personnalisée n’a pas pu être rappelée.',
+	'MIGRATION_INVALID_DATA_UNKNOWN_TYPE'			=> 'Une migration n’est pas valide. Un type d’outil de migration inconnu a été découvert.',
+	'MIGRATION_INVALID_DATA_UNDEFINED_TOOL'			=> 'Une migration n’est pas valide. Un outil de migration indéfini a été découvert.',
+	'MIGRATION_INVALID_DATA_UNDEFINED_METHOD'		=> 'Une migration n’est pas valide. Une méthode d’outil de migration indéfini a été découvert.',
 
 	'MODULE_ERROR'						=> 'Une erreur est survenue lors de la création d’un module : %s',
 	'MODULE_INFO_FILE_NOT_EXIST'		=> 'Un fichier d’information du module est manquant : %2$s',
