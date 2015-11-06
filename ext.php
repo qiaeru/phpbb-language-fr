4289c4fca3dd623d41aba5d648d5ffbd3f9a8cc0
Maël Soucaze <maelsoucaze@gmail.com>
2015-11-06 20:42:20 +0100

    Add the ext.php file.

---
+++ ext.php
@@ -0,0 +1,24 @@
+<?php
+/**
+*
+* This file is part of the official French Language Package
+* for the phpBB Forum Software.
+*
+* @copyright (c) phpBB Limited <https://www.phpbb.com>
+* @copyright (c) Maël Soucaze <https://www.maelsoucaze.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+* For information about the language package, please see
+* https://www.phpbb.com/customise/db/translation/french/
+*
+*/
+
+namespace maelsoucaze\phpbb-french;
+
+/**
+* @ignore
+*/
+
+class ext extends \phpbb\extension\base
+{
+}
