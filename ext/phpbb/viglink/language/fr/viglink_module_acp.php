6740a7fd7f02f91715b3f62c5fa46ff7946927a6
Miles Cellar <milescellar@users.noreply.github.com>
2017-07-17 13:31:49 +0200

    Update to tle latest development version

--- ext/phpbb/viglink/language/fr/viglink_module_acp.php
+++ ext/phpbb/viglink/language/fr/viglink_module_acp.php
@@ -39,6 +39,7 @@ $lang = array_merge($lang, [
 	'ACP_VIGLINK_SETTINGS'         => 'Paramètres de VigLink',
 	'ACP_VIGLINK_SETTINGS_EXPLAIN' => 'VigLink est un service tiers qui permet de monétiser en toute tranquillité les liens publiés par les utilisateurs de votre forum sans impacter leur expérience utilisateur. Lorsque les utilisateurs accèderont aux liens menant à des produits ou des services et achèteront quelque chose au marchand partenaire, ce dernier versera à VigLink une commission dont une partie du montant sera partagée au projet phpBB. En choisissant d’activer VigLink tout en laissant les parts des commissions au projet phpBB, vous aidez considérablement notre organisation de logiciels libres et gratuits en assurant notre stabilité financière.',
 	'ACP_VIGLINK_SETTINGS_CHANGE'  => 'Vous pouvez modifier ces paramètres à tout moment depuis le panneau « <a href="%1$s">Paramètres de VigLink</a> ».',
+	'ACP_VIGLINK_SUPPORT_EXPLAIN'  => 'Vous ne serez plus redirigé sur cette page lorsque vous aurez renseigné vos préférences disponibles ci-dessous, en cliquant le bouton « Envoyer ».',
 	'ACP_VIGLINK_ENABLE'           => 'Activer VigLink',
 	'ACP_VIGLINK_ENABLE_EXPLAIN'   => 'Permet l’utilisation des services de VigLink.',
 	'ACP_VIGLINK_EARNINGS'         => 'Me reverser mes parts des commissions (optionnel)',
